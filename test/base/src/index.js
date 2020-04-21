009b9863a41e127c84cbff6d552a126212420c2a
liuzhihao1 <liuzhihao1@yy.com>
2020-04-21 18:39:56 +0800

    feat: complete

---
+++ test/base/src/index.js
@@ -0,0 +1,18 @@
+const obj = {
+  'a': 3,
+  'b': 0,
+  '@': 2
+};
+
+switch (obj.a) {
+  case 1:
+    break;
+  case 2:
+    break;
+  case 3:
+    break;
+  case 4:
+    break;
+  default:
+    break;
+}
---
+++ test/base/src/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <a href="/" @click="handleClick">blah</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    handleClick() {
+      console.log('blah');
+    }
+  }
+};
+</script>
